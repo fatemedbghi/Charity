b8fff55942e980e8b1e2919e95721e45f931d0ec
fatemedbghi <fatemehdbghiii@gmail.com>
2021-03-13 22:09:58 +0330

    complete about_us

--- about_us/views.py
+++ about_us/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from accounts.models import User
 
-# Create your views here.
+def about_us(request):
+    users = User.objects.all()
+    context = {'users': users}
+    return render(request, 'about_us.html', context=context)
